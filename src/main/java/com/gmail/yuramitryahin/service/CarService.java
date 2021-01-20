260f0dc36326778e830a8afc534f7f816191efbe
Yura <yuramitryahin@gmail.com>
2021-01-20 13:27:30 +0200

    Created configuration for tomcat and servlet, also created GenericDao and GenericService

--- src/main/java/com/gmail/yuramitryahin/service/CarService.java
+++ src/main/java/com/gmail/yuramitryahin/service/CarService.java
@@ -4,16 +4,7 @@ import com.gmail.yuramitryahin.modal.Car;
 import com.gmail.yuramitryahin.modal.Driver;
 import java.util.List;
 
-public interface CarService {
-    Car create(Car car);
-
-    Car get(Long id);
-
-    List<Car> getAll();
-
-    Car update(Car car);
-
-    boolean delete(Long id);
+public interface CarService extends GenericService<Car, Long> {
 
     void addDriverToCar(Driver driver, Car car);
 
